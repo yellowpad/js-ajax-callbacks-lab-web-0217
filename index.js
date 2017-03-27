ef421cac4dd367ae13f13917f6452bd29417ccf7
dgwp-projects <zayneabraham1@gmail.com>
2017-03-27 02:41:25 -0400

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,35 @@ function handlebarsSetup() {
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
+function searchRepositories(){
+	const searchTerms = $("#searchTerms").val()
+	$.get( `https://api.github.com/search/repositories?q=${searchTerms}`, function(data){
+       const template = Handlebars.compile($('#results-template').html())
+       $('#results').html(template(data))
+	}).fail(error => {
+      displayError()
+    })
+}
+
+function showCommits(el) {
+  const owner = el.dataset.owner
+  const repo = el.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+	
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+
 $(document).ready(function (){
-  handlebarsSetup()
+  	
+  	handlebarsSetup()
+
 });
+
